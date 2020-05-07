036472461153a30aeab68bb43b7b413a77218a65
Andrew Kuznets <andrewkuznets@gmail.com>
2020-05-07 16:34:41 +0300

    Все формы сделаны

---
+++ Mater_props.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VP_CALC
+{
+    /// <summary>
+    /// Логика взаимодействия для Mater_props.xaml
+    /// </summary>
+    public partial class Mater_props : Window
+    {
+        public Mater_props()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ One_moment.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VP_CALC
+{
+    /// <summary>
+    /// Логика взаимодействия для One_moment.xaml
+    /// </summary>
+    public partial class One_moment : Window
+    {
+        public One_moment()
+        {
+            InitializeComponent();
+        }
+    }
+}
